2a7de368aae4f6703c2a1e6775e0e21671dc9fac
¨fdhortal¨ <hortalfranco@gmail.com>
2023-08-17 04:07:12 -0300

    Homework Mod 05 resuelto

---
+++ arrays.js
@@ -0,0 +1,85 @@
+// var listaDeCompras = [];
+// listaDeCompras[3] = "Tomates";
+// listaDeCompras[1] = "Lechuga";
+
+// console.log(listaDeCompras.length);
+// listaDeCompras[1];
+// var elementoDelArray = listaDeCompras[1];
+// console.log(elementoDelArray);
+
+// var nombres = ['Matias', 'Maria', 'Diego', 'Rosa'].length;
+// console.log(nombres);
+
+// Metodos de Insercion = PUSH, UNSHIFT, POP y SHIFT
+// var colores = ['amarillo', 'azul'];
+// colores.push('rojo'); // metodo para agregar al final
+// colores.unshift('verde'); // para agregar al principio
+// colores.pop(); // para eliminar elemento final
+// colores.shift(); // para eliminar elemento del principio
+// console.log(colores);
+
+// Metodo INCLUDES
+// var pintores = ['Picasso', 'Velazquez', 'Van Gogh', 'Dali'];
+// var incluyeDali = pintores.includes('Monet');
+// console.log(incluyeDali);
+
+// Metodo EVERY
+// var numeros = [10, 6, 8, 9];
+// var cumplenCondicion = numeros.every((num) => {
+//     return num > 5;
+// });
+// console.log(cumplenCondicion);
+
+// Metodos SPLIT Y JOIN (Separar y Unir)
+// var palabra = 'Henri';
+// var palabraSeparada = palabra.split('');
+// palabraSeparada.pop();
+// palabraSeparada.push('y');
+// console.log(palabraSeparada);
+// var palabraArreglada = palabraSeparada.join('');
+// console.log(palabraArreglada);
+
+// Metodo forEach
+// var numeros = [1, 2, 3, 4];
+// numeros.forEach((num) => console.log(num));
+// numeros.forEach(num => {
+//     if(num === 3) {
+//         console.log(num)
+//     }
+// });
+
+// Metodo MAP (Crea copia de arreglo original y puede realizar cambios)
+// var masUno = numeros.map((num) => {
+//     return num + 1;
+// });
+// console.log(masUno);
+
+// Bucle FOR
+// var arr = [1, 2, 3, 4, 5];
+
+// for(var i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// function encontrarLetraP(string) {
+//     var letras = string.split('');
+
+//     for(let i = 0; i < letras.length; i++) {
+//         if (letras[i] === 'p') {
+//             console.log('Si contiene a P');
+//         }
+//     }
+// }
+
+// encontrarLetraP('JavaScript');
+// encontrarLetraP('Henry');
+
+// Bucle WHILE
+// var arr = [];
+
+// while (arr.length < 5) {
+//     arr.push('BOOM');
+// }
+
+// console.log(arr);
+
